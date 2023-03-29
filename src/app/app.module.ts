9d64dfa365f223e11b5e6871efedb82a8c61101e
thamyris <thamyriscabral@hotmail.com>
2023-03-29 10:49:23 -0300

    feat: criado componente data binding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,10 @@ import { AppComponent } from './app.component';
 import { CursosModule } from './cursos/cursos.module';
 import { MeuPrimeiroComponent } from './meu-primeiro/meu-primeiro.component';
 import { MeuPrimeiro2Component } from './meu-primeiro2/meu-primeiro2.component';
+import { DataBindingComponent } from './data-binding/data-binding.component';
 
 @NgModule({
-  declarations: [AppComponent, MeuPrimeiroComponent, MeuPrimeiro2Component],
+  declarations: [AppComponent, MeuPrimeiroComponent, MeuPrimeiro2Component, DataBindingComponent],
   imports: [BrowserModule, AppRoutingModule, CursosModule],
   providers: [],
   bootstrap: [AppComponent],
