feec4d80e85e94ad998d31b37fcf1b08d8109e88
thamyris <thamyriscabral@hotmail.com>
2023-03-29 16:52:43 -0300

    feat: adcionado eventos a botoes e input

--- src/app/data-binding/data-binding.component.ts
+++ src/app/data-binding/data-binding.component.ts
@@ -7,6 +7,37 @@ import { Component } from '@angular/core';
 })
 export class DataBindingComponent {
   url: string = 'http://loiane.training';
+  cursoAngular = true;
+  urlImage = 'http://lorempixel.com.br/500/400/?2';
+
+  valorInput: string = '';
+  valorSalvo: string = '';
+
+  isMouseOver: boolean = false;
+
+  getValor() {
+    return 1;
+  }
+
+  getCurtirCurso() {
+    return 'yes';
+  }
+
+  botaoClicado() {
+    alert('cliquei');
+  }
+
+  onkeyUp(event: any) {
+    this.valorInput = event.target.value;
+  }
+
+  salvarValor(event: any) {
+    this.valorSalvo = event.value;
+  }
+
+  mouseOver() {
+    this.isMouseOver = !this.isMouseOver;
+  }
 
   constructor() {}
 }
