ac60a4bcfbb1bcf66dbb053de17a0e0e623a2380
thamyris <thamyriscabral@hotmail.com>
2023-03-30 17:03:32 -0300

    feat: exemplo ngfor

--- src/app/diretiva-ngfor/diretiva-ngfor.component.ts
+++ src/app/diretiva-ngfor/diretiva-ngfor.component.ts
@@ -3,8 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-diretiva-ngfor',
   templateUrl: './diretiva-ngfor.component.html',
-  styleUrls: ['./diretiva-ngfor.component.css']
+  styleUrls: ['./diretiva-ngfor.component.css'],
 })
 export class DiretivaNgforComponent {
-
+  cursos: string[] = ['Angular', 'React', 'Java'];
 }
